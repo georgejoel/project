6251b78a2ed3759dbcd94b8989e80a9b7d18af34
georgejoel <ubuntu@domU-12-31-39-0F-82-E1.compute-1.internal>
2013-05-03 02:45:34 +0000

    updated work with location working

--- final_project/src/com/example/final_project/MainActivity.java
+++ final_project/src/com/example/final_project/MainActivity.java
@@ -11,6 +11,10 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+
+
+
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -22,52 +26,75 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements View.OnClickListener  {
+	private Button mWeather;
+	// the listener to listen to the locations
+	private LocationListener listener = null;
+	// a location manager
+	private LocationManager lm  = null;
+	// locations instances to GPS and NETWORk
+	private Location myLocationGPS, myLocationNetwork;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	    LocationListener ll = new mylocationlistener();
-	    Intent intent = new Intent(android.content.Intent.ACTION_VIEW, 
-	    	    Uri.parse("http://maps.google.com/maps?saddr=20.344,34.34&daddr=20.5666,45.345"));
-	    	startActivity(intent);
+		lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		myLocationNetwork = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		myLocationGPS = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		listener = new myLocationListener();
+		Log.d("JG",""+myLocationNetwork.getLatitude());
+		Log.d("JG",""+myLocationNetwork.getLongitude());
+		mWeather= (Button) findViewById(R.id.button1);
+		mWeather.setOnClickListener(this);
+	    //Intent intent = new Intent(android.content.Intent.ACTION_VIEW, 
+	    //	    Uri.parse("http://maps.google.com/maps"));
+	    //	startActivity(intent);
 	}
-
+	@Override
+	public void onClick(View v) {
+		if (v == mWeather) {
+			//Log.d("JG", "click");
+			
+		} 
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		Log.d("jg","hi" );
-		Location location = null;
-		String myLocation = "Latitude = " + location.getLatitude() + " Longitude = " + location.getLongitude();
-	    Log.d("jg",myLocation);
+		
 		
 		return true;
 	}
 
-	 private class mylocationlistener implements LocationListener {
-		    @Override
-		    public void onLocationChanged(Location location) {
-		        if (location != null) {
-		        Log.d("LOCATION CHANGED", location.getLatitude() + "");
-		        Log.d("LOCATION CHANGED", location.getLongitude() + "");
-		        Log.d("jg",location.getLatitude() + "" + location.getLongitude());
-		        }
-		    }
-		    @Override
-		    public void onProviderDisabled(String provider) {
-		    }
-		    @Override
-		    public void onProviderEnabled(String provider) {
-		    }
-		    @Override
-		    public void onStatusChanged(String provider, int status, Bundle extras) {
-		    }
-		    }
+	public class myLocationListener implements LocationListener {
+	    @Override
+	    public void onLocationChanged(Location location) {
+	        // "location" is the RECEIVED locations and its here that you should proccess it
+
+	        // check if the incoming position has been received from GPS or network
+	        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+	            lm.removeUpdates(this);
+	        } else {
+	            lm.removeUpdates(listener);
+	        }
+	    }
+	    @Override
+	    public void onProviderDisabled(String provider) {
+	        lm.removeUpdates(this);
+	        lm.removeUpdates(listener);         
+	    }
+	    @Override
+	    public void onProviderEnabled(String provider) {
+	    }
+	    @Override
+	    public void onStatusChanged(String provider, int status, Bundle extras) {
+	    }
+	}
 }
 
 
